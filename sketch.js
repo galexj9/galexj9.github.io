65722379565f8f001d47210c77114e9bab0d53ad
galexj9 <43096683+galexj9@users.noreply.github.com>
2021-02-26 23:55:33 -0600

    Removed mobile functionality, adjusted sliders.

--- sketch.js
+++ sketch.js
@@ -7,20 +7,14 @@ function setup() {
   noStroke(90);
   colorMode(HSB);
 
-  let boidCount;
-  if(isMobileDevice()) {
-    createCanvas(displayWidth, displayHeight);
-    boidCount = 20;
-  } else {
-    createCanvas(window.innerWidth - 20, window.innerHeight - 20);
-    boidCount = 75;
-  }
+  let boidCount = isMobileDevice()? 20:75;
+  createCanvas(window.innerWidth - 20, window.innerHeight - 20);
 
   //default separation, alignment, and cohesion values
   sliders = createSliders(1.5, 1, 1);
 
   for(let i = 0; i < boidCount; i++)
-    boids.push(new Boid(width/2, height/2));
+    boids.push(new Boid(randomGaussian(width/2, width/10), randomGaussian(height/2, height/10)));
 
   frameRate(60);
   music.play();
@@ -32,7 +26,11 @@ function preload() {
 
 //loops once a frame to draw and run the boidos
 function draw() {
-  background(color(frameCount/6 % 360, 30 + noise(frameCount/500)*20, 60 + noise((frameCount+50)/200)*20));
+  //changes the color smoothly using color(hue, sat, lightness)
+  //noise() returns a smooth perlin noise value from 0 to 1
+  //dividing the frameCount slows how fast the color changes
+  //(360, 100, 100) -> (360, 10-50, 40-80)
+  background(color(frameCount/6 % 360, 30 + 20*noise(frameCount/500), 60 + 20*noise((frameCount+10)/200)));
 
   let power = createVector(sliders.x.value(), sliders.y.value(), sliders.z.value());
   boids.forEach(boid => boid.run(power));
@@ -45,11 +43,11 @@ function draw() {
 function createSliders(x,y,z) {
   let sepSlider, alignSlider, cohetSlider;
 
-  sepSlider = createSlider(0, 10, x, .1);
+  sepSlider = createSlider(0, 4, x, .01);
   sepSlider.position(10, 30);
   sepSlider.style('width', '80px');
 
-  alignSlider = createSlider(0, 10, y, .1);
+  alignSlider = createSlider(0, 3, y, .1);
   alignSlider.position(100, 30);
   alignSlider.style('width', '80px');
 
